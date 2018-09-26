a1f8fb9c22eed502b6098e012636f030102e3431
huskyhoochu <dfg1499@gmail.com>
2018-09-26 21:28:35 +0900

    [#2] dataConfig 파일 추가

---
+++ src/dataConfig.js
@@ -0,0 +1,6 @@
+export default {
+  baseUrl: 'http://localhost:8000',
+  fontUrl:
+    'https://fonts.googleapis.com/css?family=Nanum+Myeongjo:400,700&amp;subset=korean',
+  siteTitle: 'Littera Admin',
+};
