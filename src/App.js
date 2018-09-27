b77d4f14a89e06e175b0bde41d46b545a6c5e3d4
huskyhoochu <dfg1499@gmail.com>
2018-09-27 17:52:25 +0900

    [#3] PrivateRoute 컴포넌트 생성
    - adminToken이 없으면 SignIn 페이지로 강제 리다이렉트
    - / 로 접속해서 SignIn 페이지로 리다이렉트 시킴

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
 
 // Components
 import Loadable from './loadable';
@@ -10,14 +12,32 @@ import StyledBase from './styled/Base';
 // Minireset.css
 import '../node_modules/minireset.css/minireset.min.css';
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (sessionStorage.getItem('adminToken') ? (
+        <Component {...props} />
+    ) : (
+        <Redirect
+          to={{
+            pathname: '/sign-in',
+            state: { from: props.location },
+          }}
+        />
+    ))
+    }
+  />
+);
+
 const App = () => (
   <BrowserRouter>
     <StyledBase.App className="App">
       <Switch>
-        <Route path="/survey" component={Loadable.Survey} />
-        <Route path="/dashboard" component={Loadable.Dashboard} />
+        <PrivateRoute path="/survey" component={Loadable.Survey} />
+        <PrivateRoute path="/dashboard" component={Loadable.Dashboard} />
         <Route path="/sign-out" component={Loadable.SignOut} />
         <Route path="/sign-in" component={Loadable.SignIn} />
+        <Redirect exact from="/" to="/sign-in" />
       </Switch>
     </StyledBase.App>
   </BrowserRouter>
