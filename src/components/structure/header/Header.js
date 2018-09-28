6288f6c5e51e8f0a97903663f7bd15c1918dc6c2
huskyhoochu <dfg1499@gmail.com>
2018-09-28 16:32:48 +0900

    [#3] header title을 dataConfig에서 가져오고 Link로 변경
    - header title을 클릭하면 dashboard로 이동하도록 Link 추가

--- src/components/structure/header/Header.js
+++ src/components/structure/header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import dataConfig from '../../../dataConfig';
 
 // Styled
 import Styled from './Header.styled';
@@ -14,7 +15,9 @@ const Menu = () => (
 
 const Header = ({ visibility }) => (
   <Styled.HeaderWrapper style={{ visibility }}>
-    <h1>Littera Admin</h1>
+    <Link to="/dashboard">
+      <h1>{dataConfig.siteTitle}</h1>
+    </Link>
     <Menu />
   </Styled.HeaderWrapper>
 );
