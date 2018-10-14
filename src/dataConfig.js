abf2a4ee41b82ff80cb7ec26e6e8efe3458256d4
huskyhoochu <dfg1499@gmail.com>
2018-10-14 14:45:03 +0900

    [#4] 팝업 메시지 문구 설정
    - 헤더, 메시지, 버튼 문구 설정

--- src/dataConfig.js
+++ src/dataConfig.js
@@ -6,9 +6,23 @@ export default {
   dashboardText: '상단 헤더의 버튼을 눌러 작업을 시작하세요.',
   surveyTitle: '설문조사 관리',
   questionDetailText: '좌측 설문조사 리스트를 눌러 작업을 시작하세요.',
-  popup: {
-    destroyQuestionHeader: '질문 삭제',
-    destroyQuestionText:
-      '질문을 삭제하면 이 질문에 연결된 유저들의 응답도 모두 삭제됩니다. 계속하시겠습니까?',
+  popupMessage: {
+    destroyQuestion: {
+      header: '질문 삭제',
+      message:
+        '질문을 삭제하면 이 질문에 연결된 유저들의 응답도 모두 삭제됩니다. 계속하시겠습니까?',
+    },
+    signIn: {
+      header: 'Sign In',
+      message: 'Welcome Aboard!',
+    },
+    signOut: {
+      header: 'Sign Out',
+      message: 'Fare Well.',
+    },
+    destroyConfirm: {
+      confirm: 'delete',
+      cancel: 'cancel',
+    },
   },
 };
