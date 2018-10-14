6bfa310592dd0fe5b6b69b8eb095da3c8a446640
huskyhoochu <dfg1499@gmail.com>
2018-10-14 16:44:37 +0900

    [#4] SurveyStyled 객체들 계층구조화

--- src/components/view/survey/Survey.styled.js
+++ src/components/view/survey/Survey.styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
+import Wrapper from '../../../styled_base/Wrapper';
 
-const SurveyWrapper = styled.div`
+const SurveyWrapper = styled(Wrapper.ColumnWrapper)`
   height: calc(100vh - 4rem);
   border: 1px solid tomato;
 `;
 
-const QuestionWrapper = styled.div`
-  display: flex;
+const QuestionWrapper = styled(Wrapper.BasicFlexWrapper)`
   min-height: calc(100vh - 4rem - 2rem);
   border: 1px solid black;
 `;
